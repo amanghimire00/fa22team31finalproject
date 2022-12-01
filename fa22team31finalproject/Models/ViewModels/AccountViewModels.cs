34e8e6176729fcd4902a2bbc24136c4021d5c8b3
csparta <chris.spartalis@gmail.com>
2022-11-30 22:18:41 -0600

    updates to regsitering user

--- fa22team31finalproject/Models/ViewModels/AccountViewModels.cs
+++ fa22team31finalproject/Models/ViewModels/AccountViewModels.cs
@@ -121,5 +121,16 @@ namespace fa22team31finalproject.Models
         public String UserName { get; set; }
         public String Email { get; set; }
         public String UserID { get; set; }
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+        public String MI { get; set; }
+        public String Address { get; set; }
+        public String City { get; set; }
+        public String State { get; set; }
+        public String ZipCode { get; set; }
+        public String FullAddress { get; set; }
+        public String DOB { get; set; }
+        public String FullName { get; set; }
+        public String PhoneNumber { get; set; }
     }
 }
