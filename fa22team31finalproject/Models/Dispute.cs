8abaf1534771da8f7bd676d2b83c889aa52b87b6
Dylan Tran <dkt524@my.utexas.edu>
2022-11-30 18:08:33 -0600

    disputs model changes

--- fa22team31finalproject/Models/Dispute.cs
+++ fa22team31finalproject/Models/Dispute.cs
@@ -25,7 +25,6 @@ namespace fa22team31finalproject.Models
         [Display(Name = "Dispute Description")]
         public String DisputeDescription { get; set; }
 
-        public TransactionDetail TransactionDetail { get; set; }
         public Transaction Transaction { get; set; }
     }
 }
